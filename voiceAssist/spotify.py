819a1b5e7bd5697c4c08481094f7b1a637471966
chandyego <chandler.juego@wsu.edu>
2021-10-17 09:46:11 -0700

    hid keys

--- voiceAssist/spotify.py
+++ voiceAssist/spotify.py
@@ -1,11 +1,12 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from pprint import pprint
+import os
 from time import sleep
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="5ceba33083f9440aa4b1d56878faa463",
-                                               client_secret="772c980cdc63400a9618d6cf35495bb8",
-                                               redirect_uri="https://www.google.com/",
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ['SPOTIPY_CLIENT_ID'],
+                                               client_secret=os.environ['SPOTIPY_CLIENT_SECRET'],
+                                               redirect_uri=os.environ['SPOTIPY_REDIRECT_URI'],
                                                scope="user-read-playback-state,user-modify-playback-state"))
 
 res = sp.devices()
